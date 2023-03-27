ecd0414208406400b8c50fe9f2f3599726d5bc55
Vignesh Esakkiappan <85873362+Papavig@users.noreply.github.com>
2023-03-27 21:46:51 +0530

    first commit

---
+++ assets/js/commands.js
@@ -0,0 +1,55 @@
+var linkedin = "https://www.linkedin.com/in/papavig/";
+var instagram = "https://www.instagram.com/beingvig/";
+var github = "https://github.com/Papavig";
+var email = 'mailto:being@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Vignesh Esakkiappan",
+  "A Devloper/Student",
+  "I’m a student trying to improve my skills in building (and occasionally designing) web apps and sites.",
+  "I'm passionate about creating efficient and scalable solutions to complex problems, ",
+  "All coding projects are built from the ground up, from planning and designing all the way to solving real-life problems with code,",
+  "and I'm always looking for opportunities to collaborate with other developers. ",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/Papavig' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/beingvig' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/Papavig' + "</a>",
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Working on it :)",
+  "most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Vignesh Esakkiappan?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">WebShell is Not A Corporation. All knights reserved.</span>',
+  "_       __     __   _____ __         ____",
+  "| |     / /__  / /_ / ___// /_  ___  / / /",
+  "| | /| / / _ \/ __ \\__ \/ __ \/ _ \/ / / ",
+  "| |/ |/ /  __/ /_/ /__/ / / / /  __/ / /  ",
+  "|__/|__/\___/_.___/____/_/ /_/\___/_/_/  ", 
+   "                                       ",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
